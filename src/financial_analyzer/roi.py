11d1557ad096a3678a50c2a1383145c2736d707a
Rafe Chang <cchang5@student.ubc.ca>
2024-01-12 13:35:13 -0800

    name update

---
+++ src/financial_analyzer/roi.py
@@ -0,0 +1,18 @@
+def roi(initial_investment, current_value):
+    """ Calculate the Return on Investment (ROI).
+    
+    Parameters 
+    ----------
+    initial_investment : float
+        The initial amount invested.
+    current_value : float 
+        The current value of the investment.
+    
+    Returns
+    --------
+    float: The ROI expressed as a percentage.
+    
+    Examples 
+    --------
+    >>> calculate_roi(1000000, 1200000)
+    """
