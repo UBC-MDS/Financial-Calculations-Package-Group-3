21bc5ce722852e2b8010c627d7e509b9dbde0517
Nicole Tu <yejun.tu1202@gmail.com>
2024-01-11 19:49:37 -0800

    function file added

---
+++ src/financial_analyzer/units_for_profit.py
@@ -0,0 +1,14 @@
+def calculate_units_for_target_profit(fixed_costs, sales_price_per_unit, variable_cost_per_unit, desired_profit):
+    """
+    Calculate the number of units needed to be sold to achieve a desired profit.
+
+    Parameters:
+    fixed_costs (float): Total fixed costs.
+    sales_price_per_unit (float): Selling price of each unit.
+    variable_cost_per_unit (float): Variable cost of each unit.
+    desired_profit (float): Target profit.
+
+    Returns:
+    float: Number of units needed to be sold to achieve the desired profit.
+    """
+    
