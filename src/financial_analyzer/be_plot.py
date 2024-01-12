30397bbc0c8e40c80f98c762b0289fb09765496d
Chris Gao <chrisgaoqy@gmail.com>
2024-01-11 19:35:36 -0800

    Add plotting function documentation: visualize break even point

---
+++ src/financial_analyzer/be_plot.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+
+def plot_break_even_point(fixed_costs, sales_price_per_unit, variable_cost_per_unit, max_units):
+    """
+    Plot the break-even point.
+    Parameters:
+    fixed_costs (float): Total fixed costs.
+    sales_price_per_unit (float): The selling price of each unit.
+    variable_cost_per_unit (float): The variable cost of each unit.
+    max_units (int): Maximum number of units to plot.
+    Returns:
+    None: The function will plot the break-even graph.
+    """
