298d042a361b2d7534d22de522b18735a0a8b61e
Chris Gao <chrisgaoqy@gmail.com>
2024-01-19 18:45:06 -0800

    1st update of test function

--- tests/test_plot_break_even_point.py
+++ tests/test_plot_break_even_point.py
@@ -1,5 +1,6 @@
 from finanancial_analyzer.plot_breakeven_point import plot_breakeven_point
 import numbers
+import pytest
 
 
 def test_graph_type():
@@ -11,7 +12,7 @@ def test_graph_type():
     variable_cost_per_unit = 0.5
     max_unit = 10
 
-    sample_plot = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
+    sample_plot, _ = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
 
     assert str(type(sample_plot)) == "<class 'altair.vegalite.v5.api.LayerChart'>", \
         "The output should be a layered chart"
@@ -25,7 +26,7 @@ def test_graph_depth():
     variable_cost_per_unit = 0.5
     max_unit = 10
 
-    sample_plot = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
+    sample_plot, _ = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
 
     assert len(sample_plot.layer) == 9, f"There should be 9 layers, instead of {len(sample_plot.layer)}" 
 
@@ -37,7 +38,7 @@ def test_graph_each_layer():
     variable_cost_per_unit = 0.5
     max_unit = 10
 
-    sample_plot = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
+    sample_plot, _ = plot_breakeven_point(fixed_cost, sales_price_per_uni, variable_cost_per_unit, max_unit)
 
     for i, layer in enumerate(sample_plot.layer):
         assert isinstance(layer, alt.Chart), f"Layer {i+1} is not an Altair Chart"
